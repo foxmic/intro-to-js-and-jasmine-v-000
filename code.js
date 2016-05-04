15da3e99da1d1878c15008c7e48ada02979aea9a
Fox Mic <michelfuchs0@gmail.com>
2016-05-04 11:17:30 +1000

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(name) {
+  return "I love " + name;
+}
+
+function shouting(word) {
+  return word.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function theTruth() {
+  return true;
+}
